a7f28737a26b8feacb2eec04a1d05b20f274a34d
maria <0rini0mk@gmail.com>
2023-02-21 22:24:47 +0300

    laba 2 finished

---
+++ dz2/back.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Лабораторная №2</title>
+</head>
+<body>
+    <header>
+        <div class="header_wrapper">
+            <img src="img/123.jpg" alt="logo">
+            <h1>ДЗ №2 Feedback form</h1>
+        </div>
+    </header>
+    <main class="function">
+        <textarea class="function_text" name="" id="" cols="50" rows="30">
+            <?php print_r(get_headers('https://www.php.net/releases/8.2/en.php')) 
+            ?>
+        </textarea>
+    </main>
+    <footer>
+        <p>Выполнила студентка группы 221-322 Кожаринова Мария Павловна</p>
+    </footer>
+</body>
+
+</html>
