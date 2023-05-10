b16d8ff19b70a9aa372b80d3fe26ca71bd6f88dc
maria <0rini0mk@gmail.com>
2023-05-11 02:34:35 +0300

    coursework

--- coursework/db.php
+++ coursework/db.php
@@ -1,4 +1,4 @@
 <?php 
-$db = mysqli_connect("https://phpmyadmin.fit.mospolytech.ru", "std_2148_coursework", "MashaKim04", "std_2148_coursework");
+$db = mysqli_connect("std-mysql", "std_2148_coursework", "MashaKim04", "std_2148_coursework");
 $db->set_charset("utf8");
 ?>
